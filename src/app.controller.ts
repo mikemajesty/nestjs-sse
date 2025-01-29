3592a67f5795edf3f727b2120f1161251b41cd10
Mike Lime <mike.rodrigues.lima@gmail.com>
2025-01-29 13:42:47 -0300

    add user id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, MessageEvent, Res, Sse } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Param, Res, Sse } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Response } from 'express';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import {  Observable } from 'rxjs';
+
+const EVENT = "observando";
 
 @Controller()
 export class AppController {
@@ -16,16 +18,17 @@ export class AppController {
       .send(readFileSync(join(__dirname, 'index.html')).toString());
   }
 
-  @Get("emit")
-  emit() {
-    //http://localhost:3002/sse emitir um evento por essa rota
-    this.eventEmitter.emit("observando", { info: "email atuaizado com sucesso" })
+  @Get("emit/:id")
+  emit(@Param("id") id: string) {
+    //http://localhost:3002/emit/2 emitir um evento por essa rota
+    //http://localhost:3002/emit/1 emitir um evento por essa rota
+    this.eventEmitter.emit(EVENT.concat(id), { info: "email atuaizado com sucesso" })
   }
 
-  @Sse('sse')
-  sse(): Observable<MessageEvent> {
+  @Sse('sse/:id')
+  sse(@Param("id") id: string): Observable<MessageEvent> {
     return new Observable((subscriber) => {
-      this.eventEmitter.on('observando', (data) => {
+      this.eventEmitter.on(EVENT.concat(id), (data) => {
         //envia evento para o front end
         subscriber.next({ data });
       });
